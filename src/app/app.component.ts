ee4f4268f89dd6c0e539f64c13113dd53724759f
Ryan Balfanz <ryan@ryanbalfanz.net>
2016-12-01 23:00:39 -0800

    Add submit button to create channel form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  addChannel(name: HTMLInputElement): boolean {
+    console.log(`Adding channel name: ${name.value}`);
+    return false;
+  }
 }
