0d7ff103be30d19b402be1b98b41ba6a308f25a3
Ryan Balfanz <ryan@ryanbalfanz.net>
2016-12-04 16:06:28 -0800

    Use Firebase database to store and retrieve channels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AngularFireModule } from 'angularfire2';
+
 import { AppComponent } from './app.component';
 import { ChannelFactory } from './channel.factory';
 import { ChannelListComponent } from './channel-list/channel-list.component';
 import { ChannelItemComponent } from './channel-item/channel-item.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBOfR3VVMOJstKuar19gIshoU1dCTylXa4',
+  authDomain: '<your-project-authdomain>',
+  databaseURL: 'https://bloc-chat-ng2.firebaseio.com/',
+  storageBucket: '<your-storage-bucket>'
+};
 
 @NgModule({
   declarations: [
@@ -17,7 +26,8 @@ import { ChannelItemComponent } from './channel-item/channel-item.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [ChannelFactory],
   bootstrap: [AppComponent]
