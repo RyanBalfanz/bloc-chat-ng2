1aa0abd23576a22a26a57de302ad052500a52d19
Ryan Balfanz <ryan@ryanbalfanz.net>
2016-12-12 00:43:28 -0800

    Add messages input

--- src/app/message-list/message-list.component.ts
+++ src/app/message-list/message-list.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit
+} from '@angular/core';
+
+import { Message } from '../message.model';
 
 @Component({
   selector: 'app-message-list',
   templateUrl: './message-list.component.html',
-  styleUrls: ['./message-list.component.css']
+  styleUrls: ['./message-list.component.css'],
 })
 export class MessageListComponent implements OnInit {
+  @Input() messages: Message[] = [];
 
   constructor() { }
 
