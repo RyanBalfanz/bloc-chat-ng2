00b0cc2bba7e010185157935471cb141faa3b34e
Ryan Balfanz <ryan@ryanbalfanz.net>
2016-12-14 21:12:20 -0800

    Refactor router into module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,31 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
 import { ChannelFactory } from './channel.factory';
 import { MessageFactory } from './message.factory';
 import { ChannelListComponent } from './channel-list/channel-list.component';
 import { ChannelItemComponent } from './channel-item/channel-item.component';
 import { MessageListComponent } from './message-list/message-list.component';
-
-const appRoutes: Routes = [
-  {
-    path: '',
-    redirectTo: '/channels',
-    pathMatch: 'full'
-  },
-  {
-    path: 'channels',
-    component: ChannelListComponent,
-    data: {
-      title: 'Channel List'
-    }
-  },
-];
 
 export const firebaseConfig = {
   apiKey: 'AIzaSyBOfR3VVMOJstKuar19gIshoU1dCTylXa4',
@@ -46,7 +32,7 @@ export const firebaseConfig = {
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes),
+    AppRoutingModule,
     AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [ChannelFactory, MessageFactory],
