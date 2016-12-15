1161e24f450764dc72c35710f17e1c1a4dbfbad0
Ryan Balfanz <ryan@ryanbalfanz.net>
2016-12-14 21:04:37 -0800

    Add initial router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AngularFireModule } from 'angularfire2';
 
@@ -11,6 +12,21 @@ import { MessageFactory } from './message.factory';
 import { ChannelListComponent } from './channel-list/channel-list.component';
 import { ChannelItemComponent } from './channel-item/channel-item.component';
 import { MessageListComponent } from './message-list/message-list.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/channels',
+    pathMatch: 'full'
+  },
+  {
+    path: 'channels',
+    component: ChannelListComponent,
+    data: {
+      title: 'Channel List'
+    }
+  },
+];
 
 export const firebaseConfig = {
   apiKey: 'AIzaSyBOfR3VVMOJstKuar19gIshoU1dCTylXa4',
@@ -30,6 +46,7 @@ export const firebaseConfig = {
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot(appRoutes),
     AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [ChannelFactory, MessageFactory],
